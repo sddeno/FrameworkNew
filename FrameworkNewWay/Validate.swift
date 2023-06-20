df6c2caef842738703c5791f4b2d57950b191f4b
Shubham Deshmukh <shubhamdeshmukh69@gmail.com>
2023-06-20 19:45:00 +0530

    initial commit

---
+++ FrameworkNewWay/Validate.swift
@@ -0,0 +1,21 @@
+//
+//  Validate.swift
+//  FrameworkNewWay
+//
+//  Created by Shubham Deshmukh on 20/06/23.
+//
+
+import Foundation
+
+
+public class Validate {
+    
+    func SayHello(){
+        print("how are you ?")
+    }
+    
+    func PrintName(name: String){
+        print("Hello \(name)")
+    }
+}
+
